5e881a84f5d2452f8bab70cdd2ec6236929d5cc1
Arian Hakimzadeh <nikobalek1381@gmail.com>
2024-08-18 15:14:41 +0330

    Add Flip Effect

--- Assets/Scripts/Platform.cs
+++ Assets/Scripts/Platform.cs
@@ -7,13 +7,21 @@ public class Platform : MonoBehaviour
 {
     [SerializeField]
     private float jumpSpeed = 15f;
+
+    private Rigidbody2D platfrom;
     
     private void OnCollisionEnter2D(Collision2D other)
     {
         if (other.relativeVelocity.y <= 0)
         {
+            platformAnim();
             Rigidbody2D rb =  other.transform.GetComponent<Rigidbody2D>();
             rb.velocity = new Vector2(0,jumpSpeed);
         }
+    }
+
+    void platformAnim()
+    {
+        platfrom = this.GetComponent<Rigidbody2D>();
     }
 }
--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -1,18 +1,41 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Rigidbody2D player;
+    public float speed = 15f;
+    private float _direction;
+    private bool playerSprite;
+    
     void Start()
     {
+        player = GetComponent<Rigidbody2D>();
+        playerSprite = player.transform.GetComponent<SpriteRenderer>().flipX;
+    }
+    
+    void Update()
+    {
+        _direction = Input.GetAxis("Horizontal");
+        
+        if (_direction > 0)
+        {
+            player.transform.GetComponent<SpriteRenderer>().flipX = true;
+        }
+        else if (_direction < 0)
+        {
+            
+            player.transform.GetComponent<SpriteRenderer>().flipX = false;
+        }
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   void FixedUpdate()
+   {
+       var velocity = player.velocity;
+       velocity.x = _direction * speed;
+       player.velocity = velocity ;
+   }
 }
