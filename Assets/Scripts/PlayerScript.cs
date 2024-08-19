02a03256bb502401b39b8b5e78bd22b3f0884462
Arian Hakimzadeh <nikobalek1381@gmail.com>
2024-08-19 13:24:02 +0330

    Add Going off Camera View

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -6,30 +6,22 @@ using UnityEngine;
 public class PlayerScript : MonoBehaviour
 {
     private Rigidbody2D player;
+    private SpriteRenderer playerSprite;
     public float speed = 15f;
     private float _direction;
-    private bool playerSprite;
+    private float screenWidth;
+    private float _cameraOffset = 0.25f;
     
     void Start()
     {
         player = GetComponent<Rigidbody2D>();
-        playerSprite = player.transform.GetComponent<SpriteRenderer>().flipX;
+        playerSprite = player.transform.GetComponent<SpriteRenderer>();
+        screenWidth = Camera.main.aspect * Camera.main.orthographicSize + _cameraOffset;
     }
     
     void Update()
     {
-        _direction = Input.GetAxis("Horizontal");
-        
-        if (_direction > 0)
-        {
-            player.transform.GetComponent<SpriteRenderer>().flipX = true;
-        }
-        else if (_direction < 0)
-        {
-            
-            player.transform.GetComponent<SpriteRenderer>().flipX = false;
-        }
-        
+        CalculateMovement();
     }
 
    void FixedUpdate()
@@ -37,5 +29,36 @@ public class PlayerScript : MonoBehaviour
        var velocity = player.velocity;
        velocity.x = _direction * speed;
        player.velocity = velocity ;
+   }
+    void CalculateMovement()
+    {
+        ChangeDirection();
+        IsOut();
+    }
+    void IsOut()
+   {
+       if (transform.position.x > screenWidth)
+       {
+           transform.position = new Vector3(-screenWidth, transform.position.y, transform.position.z);
+       }
+       else if (transform.position.x < -screenWidth)
+       {
+           transform.position = new Vector3(screenWidth, transform.position.y, transform.position.z);
+       }
+   }
+
+   void ChangeDirection()
+   {
+       _direction = Input.GetAxis("Horizontal");
+        
+       if (_direction > 0)
+       {
+           playerSprite.flipX = true;
+       }
+       else if (_direction < 0)
+       {
+            
+           playerSprite.flipX = false;
+       }
    }
 }
