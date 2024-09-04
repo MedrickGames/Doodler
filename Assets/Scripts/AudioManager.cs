6b9075b3f49e689171822bc4f8f205a58fa70426
Arian Hakimzadeh <nikobalek1381@gmail.com>
2024-09-04 10:18:47 +0330

    Add Audio

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,41 @@
+using System;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class AudioManager : MonoBehaviour
+{
+    public AudioClip[] audioClips; // Populate this array in the Inspector
+
+    private AudioSource audioSource;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player") && other.relativeVelocity.y <= 0 || other.relativeVelocity.y > 0 && other.relativeVelocity.y < 1) // Make sure to use the correct tag
+        {
+            PlayRandomAudio();
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player")) // Make sure to use the correct tag
+        {
+            PlayRandomAudio();
+        }
+    }
+
+    private void PlayRandomAudio()
+    {
+        if (audioClips.Length > 0)
+        {
+            int randomIndex = Random.Range(0, audioClips.Length);
+            audioSource.clip = audioClips[randomIndex];
+            audioSource.Play();
+        }
+    }
+}
